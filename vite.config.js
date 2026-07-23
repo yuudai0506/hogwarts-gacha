d9b3b0ce1bf7d8f8e3f9328f0e79f94004497d9f
yaname <241117@st.yoshida-g.ac.jp>
2026-07-23 09:51:51 +0900

    再２

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@viteplugin-react' // または @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hogwarts-gacha/', // ← 🌟 この行を追加してください！（リポジトリ名と同じにする）
+  base: '/hogwarts-gacha/', // ← 画面真っ白を防ぐ設定
 })
